46f9d22831d596e2684e65c52bb8214d8d6761a6
ngallodev <49315035+ngallodev@users.noreply.github.com>
2026-09-24 21:01:01 -0700

    feat: expose neutral study bundle validators

--- src/agent_workflow_comparative_eval/__init__.py
+++ src/agent_workflow_comparative_eval/__init__.py
@@ -17,6 +17,8 @@ from .errors import ComparativeEvalError, ContractError, CohortError, ImmutableO
 from .study import (
     list_studies,
     load_study_spec,
+    validate_decision_study_corpus,
+    validate_decision_study_oracle_bundle,
     make_provider_request,
     make_exclusion,
     make_precomputed_decision_observation,
